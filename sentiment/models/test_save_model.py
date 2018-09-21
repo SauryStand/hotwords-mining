25fbf991c0782bcb0c763e22f243ce3d949bb2fc
johnny chow <voyager2511@johnnys-MacBook-Pro.local>
2018-09-21 14:16:19 +0800

    random update

--- sentiment/models/test_save_model.py
+++ sentiment/models/test_save_model.py
@@ -11,3 +11,8 @@ def get_features(data, postags):
     data_feature = pre_process(data, postags)  # 这里data 每一行已经分词了
     print data_feature.shape
     return data_feature
+
+
+def main():
+    train_data, train_target, train_pos = read_train_data('2013')
+    train_feature = get_features(train_data, train_pos)
