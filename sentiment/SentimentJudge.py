e75342114f9acaf6a263bd4b5e72c245e691f7d4
johnny chow <voyager2511@johnnys-MacBook-Pro.local>
2018-09-30 17:34:05 +0800

    update: singleton in model_save

--- sentiment/SentimentJudge.py
+++ sentiment/SentimentJudge.py
@@ -2,6 +2,8 @@
 
 
 from scipy.sparse import csr_matrix
+from sklearn import metrics
+from sklearn.externals import joblib
 
 class SentimentJudge(object):
     """
@@ -11,9 +13,20 @@ class SentimentJudge(object):
             predicted = s.predict(test_data)
             print np.sum(predicted == _test_target), len(_test_target), np.mean(predicted == _test_target)
     """
-    __metaclass__ = Singletongit
+    __metaclass__ = Singleton
 
+    def __init__(self):
+        self.classifier = joblib.load('sentiment/models/models_save/classifier')
+        self.ngram = joblib.load('sentiment/models/models_save/ngrams')
+        self.lexicon = joblib.load('sentiment/models/models_save/lexicon')
 
+    def predict(self, X):
+        """
+            Predict X is positive or negative
+        :param X:
+        :return: a numpy.ndarray. each row with "positive" or "negative"
+        """
+        return self.classifier.predict(X)
 
 
 def main():
