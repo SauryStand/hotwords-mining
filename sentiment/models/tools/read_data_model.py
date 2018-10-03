e09a9f8a3e473ce96c2689a3bf1987b928cc6ca5
wohuodong <janfongz@126.com>
2018-10-03 22:53:18 +0800

    update: nlp import

---
+++ sentiment/models/tools/read_data_model.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+import codecs
+import subprocess
+import sys
+
+
+def create_pos_file(path):
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+    print('read data from', path)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
