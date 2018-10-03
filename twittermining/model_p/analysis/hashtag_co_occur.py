f6f08eabdf9cca3d59b30c7df501ad91bf2baa01
wohuodong <janfongz@126.com>
2018-10-03 09:52:17 +0800

    update: sample model update

--- twittermining/model_p/analysis/hashtag_co_occur.py
+++ twittermining/model_p/analysis/hashtag_co_occur.py
@@ -13,4 +13,27 @@ if __name__ == '__main__':
     upper_bound = str(int(time.mktime(datetime.datetime(2015, 11, 17).timetuple())) * 1000)
     client = pymongo.MongoClient()
     db = client.twitter
-    cursor = db.stream.aggregate()
+    cursor = db.stream.aggregate([
+        {
+            '$match': {
+                'timestamp_ms': {
+                    '$gt': lower_bound,
+                    '$lt': upper_bound,
+                },
+                'entities.hashtags.0': {
+                    '$exists': 'true'
+                }
+            }
+        },
+        {
+            '$project': {
+                'entities.hashtags': 1
+            }
+        }
+    ])
+    cnt = 0
+    hashtag_dic = collections.defaultdict(lambda: collections.defaultdict(int), {})
+    for tweet in cursor:
+        cnt += 1
+        hashtags = tweet['entities']['hashtags']
+        hashtags_len = len(hashtags)
